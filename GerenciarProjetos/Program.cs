aad168e96b19e020032b0f1681c6555e04cbc9b1
Pedro <pedrohbribeiro42@gmail.com>
2022-09-29 21:18:14 -0300

    Auto apply migrations

--- GerenciarProjetos/Program.cs
+++ GerenciarProjetos/Program.cs
@@ -97,4 +97,15 @@ app.UseAuthentication();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<Context>();
+    if (context.Database.GetPendingMigrations().Any())
+    {
+        context.Database.Migrate();
+    }
+}
+
 app.Run();
